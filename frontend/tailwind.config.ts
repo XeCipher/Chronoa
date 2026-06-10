ae7e8902c142ed3930381c7fb7bc97e64b4fc75f
Chaitanya Patil <chaitanyapatil.xe@gmail.com>
2026-06-10 15:47:15 +0530

    chore: set package.json type to module and refactor tailwind require to import to eliminate Vercel build warnings

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: 'class',
@@ -15,7 +16,8 @@ const config: Config = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 };
+
 export default config;
